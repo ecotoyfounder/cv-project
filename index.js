7095ac8b15d0e1e0f87cf8d844367f757d442098
Pavel <101211926+ecotoyfounder@users.noreply.github.com>
2022-04-03 18:48:07 +0300

    My 1st project is here!

---
+++ index.js
@@ -0,0 +1,20 @@
+function theme() {
+  const toggleTheme = document.querySelector('.toggle-theme')
+  let el = document.documentElement
+
+  toggleTheme.addEventListener('click', () => {
+    if (el.hasAttribute('data-theme')) {
+      el.removeAttribute('data-theme')
+      localStorage.removeItem('theme')
+    } else {
+      el.setAttribute('data-theme', 'dark')
+      localStorage.setItem('theme', 'dark')
+    }
+  })
+
+  if (localStorage.getItem('theme') !== null) {
+    el.setAttribute('data-theme', 'dark')
+  }
+}
+
+theme()
